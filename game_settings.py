96e7d2bd56ea177481f954079ae2ea13572e371a
niklas-kessler <95214248+niklas-kessler@users.noreply.github.com>
2023-03-21 09:23:11 +0100

    enhance draw_boundaries

--- game_settings.py
+++ game_settings.py
@@ -12,7 +12,8 @@ class GameSettings:
     WINDOW_HEIGHT = 600
     WINDOW_WIDTH = 800
 
-    BOUNDARY_COLOR = (180,180,0,255)
+    BOUNDARY_COLOR = (180, 180, 0, 255)
+    MOUSE_CLICK_HIT_BOX = 15
 
     def __init__(self, game_status=GameStatus.DRAW_BOUNDARIES):
         self.GAME_STATUS = game_status
--- racetrack.py
+++ racetrack.py
@@ -1,6 +1,6 @@
+import math
 import pyglet as pg
 from pyglet.window import mouse
-
 from game_settings import *
 
 
@@ -9,6 +9,10 @@ def print_boundaries(bound_arr):
         print("visible: {}, start: {}, end: ({},{})".format(boundary.visible,
                                                             boundary.position,
                                                             boundary.x2, boundary.y2))
+
+def mouse_hit_box(x, y, mouse_x, mouse_y):
+    """This method checks, if the mouse click approximately hit a point."""
+    return math.sqrt((mouse_x - x)**2 + (mouse_y - y)**2) < GameSettings.MOUSE_CLICK_HIT_BOX
 
 
 class Racetrack(pg.sprite.Sprite):
@@ -22,20 +26,46 @@ class Racetrack(pg.sprite.Sprite):
         self.x = GameSettings.WINDOW_WIDTH // 2
         self.y = GameSettings.WINDOW_HEIGHT // 2
         self.boundaries = []
-        self.boundary_start_coord = None
+        self.boundaries_start_coord = None
+        self.boundary_curr_start_coord = None
         self.racetrack_batch = pg.graphics.Batch()
 
     def on_mouse_press(self, x, y, button, modifiers):
-        if self.boundary_start_coord is None:
-            print("draw_boundaries (1/2)")
-            self.boundary_start_coord = (x, y)
-        else:
-            if button == mouse.LEFT:
-                print("draw_boundaries (2/2)")
-                boundary = pg.shapes.Line(self.boundary_start_coord[0],
-                                          self.boundary_start_coord[1], x, y,
+        if button == mouse.RIGHT:
+            # remove last boundary
+            print("remove last boundary")
+            if self.boundaries:
+                b = self.boundaries.pop()
+                self.boundary_curr_start_coord = (b.x, b.y)
+
+        elif button == mouse.LEFT:
+
+            if self.boundary_curr_start_coord is None:
+                print("boundaries start point")
+                self.boundaries_start_coord = (x, y)
+                self.boundary_curr_start_coord = (x, y)
+
+            elif mouse_hit_box(self.boundaries_start_coord[0],
+                               self.boundaries_start_coord[1], x, y ):
+                # draw last boundary, set end point to first start point
+                print("final boundary")
+                b = pg.shapes.Line(self.boundary_curr_start_coord[0],
+                                   self.boundary_curr_start_coord[1],
+                                   self.boundaries_start_coord[0],
+                                   self.boundaries_start_coord[1],
+                                   color=GameSettings.BOUNDARY_COLOR,
+                                   batch=self.racetrack_batch)
+                self.boundaries.append(b)
+                self.boundaries_start_coord = None
+                self.boundary_curr_start_coord = None
+
+            else:
+                # draw next boundary
+                print("next boundary")
+                boundary = pg.shapes.Line(self.boundary_curr_start_coord[0],
+                                          self.boundary_curr_start_coord[1], x, y,
                                           color=GameSettings.BOUNDARY_COLOR,
                                           batch=self.racetrack_batch)
                 self.boundaries.append(boundary)
-                print_boundaries(self.boundaries)
-            self.boundary_start_coord = None
+                self.boundary_curr_start_coord = (x, y)
+            print_boundaries(self.boundaries)
